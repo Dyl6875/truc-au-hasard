6a9618a9bb16dbc05d1c2c2add21bc11cfd9f21f
Dyl6875 <132678439+Dyl6875@users.noreply.github.com>
2023-05-12 16:00:17 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showIcon(IconNames.Pitchfork)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showIcon(IconNames.Skull)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Scissors)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showIcon(IconNames.Ghost)
+})
