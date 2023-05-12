d893df89337a13575112802bccdf06d8175680fa
Dyl6875 <132678439+Dyl6875@users.noreply.github.com>
2023-05-12 16:31:56 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        `)
+})
 input.onGesture(Gesture.TiltRight, function () {
     basic.showIcon(IconNames.Pitchfork)
 })
@@ -7,6 +16,18 @@ input.onGesture(Gesture.TiltLeft, function () {
 input.onGesture(Gesture.Shake, function () {
     basic.showIcon(IconNames.Scissors)
 })
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . # . # .
+        . . . . .
+        . . # . .
+        `)
+})
 input.onGesture(Gesture.ScreenUp, function () {
     basic.showIcon(IconNames.Ghost)
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    basic.showIcon(IconNames.Duck)
 })
